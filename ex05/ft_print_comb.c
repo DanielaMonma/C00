e92af4bfa36982b7a780892f6d8d8249492a3238
Daniela Pereira Monma <danielpe@c2r9p1.42sp.org.br>
2024-08-26 21:27:38 -0300

    C00 - o retorno

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -6,44 +6,45 @@
 /*   By: danielpe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 17:53:01 by danielpe          #+#    #+#             */
-/*   Updated: 2024/08/26 19:04:11 by danielpe         ###   ########.fr       */
+/*   Updated: 2024/08/26 20:59:15 by danielpe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_print(char n1, char n2, char n3)
+void	ft_print(char n1, char n2, char n3)
 {
-    write(1, &n1, 1);
-    write(1, &n2, 1);
-    write(1, &n3, 1);
-    if (n1 != '7' || n2 != '8' || n3 != '9')
-    {
-        write(1, ", ", 2);
-    }
+	write (1, &n1, 1);
+	write (1, &n2, 1);
+	write (1, &n3, 1);
+	if (n1 != '7' || n2 != '8' || n3 != '9')
+	{
+		write(1, ", ", 2);
+	}
 }
 
-void ft_print_comb(void)
+void	ft_print_comb(void)
 {
-    char n1 = '0';
-    char n2;
-    char n3;
+	char	n1;
+	char	n2;
+	char	n3;
 
-    while (n1 <= '7')
-    {
-        n2 = n1 + 1;
-        while (n2 <= '8')
-        {
-            n3 = n2 + 1;
-            while (n3 <= '9')
-            {
-                ft_print(n1, n2, n3);
-                n3++;
-            }
-            n2++;
-        }
-        n1++;
-    }
+	n1 = '0';
+	while (n1 <= '7')
+	{
+		n2 = n1 + 1;
+		while (n2 <= '8')
+		{
+			n3 = n2 + 1;
+			while (n3 <= '9')
+			{
+				ft_print(n1, n2, n3);
+				n3++;
+			}
+				n2++;
+		}
+			n1++;
+	}
 }
 /*
 int main ()
